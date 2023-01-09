d60108005d468145a6e0666ee1ba703852ad1bd4
Adrian McAllister <amcallister2317@student.pps.net>
2023-01-09 15:25:42 -0800

    Piece Move Skeleton

---
+++ Assets/Scripts/PieceMove.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMove : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void PawnMove()
+    {
+
+    }
+
+    void BishopMove()
+    {
+
+    }
+
+    void KnightMove()
+    {
+
+    }
+
+    void RookMove()
+    {
+
+    }
+
+    void QueenMove()
+    {
+
+    }
+
+    void KingMove()
+    {
+
+    }
+}
