0bf93767f1b7dfc5011f64e34bc7bd62b4dc79f3
Adrian McAllister <amcallister2317@student.pps.net>
2023-01-11 14:46:57 -0800

    Added Tags

--- Assets/Scripts/PieceMove.cs
+++ Assets/Scripts/PieceMove.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class PieceMove : MonoBehaviour
 {
+    
     void Start()
     {
         
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            Debug.Log("Mouse Clicked");
+
+            if (hit.collider != null) 
+            {
+                
+            Debug.Log("Something was clicked!");
+            }
+        }
     }
 
     void PawnMove()
